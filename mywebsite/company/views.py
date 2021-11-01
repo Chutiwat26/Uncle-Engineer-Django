dcd9147e76e68a018b6cf1317ac42c8fb20e0597
Chutiwat Tanapibalwongsa <62974118+Chutiwat26@users.noreply.github.com>
2021-11-01 18:34:27 +0700

    done ep2

--- mywebsite/company/views.py
+++ mywebsite/company/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse # import เพื่อให้สามารถแสดงผลบนหน้าเว็บได้
 
 # Create your views here.
+def Home(request):
+    return HttpResponse('<h1>Hello World!</h1> <br> <p>My name is chutiwat</p>')
--- mywebsite/mywebsite/urls.py
+++ mywebsite/mywebsite/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include #inculde คือคำสั่งสำหรับลิงค์ระหว่าง project กับ app
+
+#import created app
+import company
+from company import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(company.urls)), #project link to 'company'
 ]
